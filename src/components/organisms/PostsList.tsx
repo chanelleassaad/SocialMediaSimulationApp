090382ac0f7fff8d79f852cf0f9f48fe0476ca7b
Chanelle Assaad <assaadchanelle@gmail.com>
2024-04-09 13:17:38 +0300

    Optimize list rendering Add reactotron

--- src/components/organisms/PostsList.tsx
+++ src/components/organisms/PostsList.tsx
@@ -1,13 +1,19 @@
-import React from 'react';
+import React, {useCallback} from 'react';
 import {FlatList} from 'react-native';
 import PostItem from '../molecules/PostItem';
 
 const PostsList = ({posts}) => {
+  const renderItem = useCallback(({item}) => {
+    return <PostItem item={item} />;
+  }, []);
+
   return (
     <FlatList
       data={posts}
-      keyExtractor={item => item.id}
-      renderItem={({item}) => <PostItem item={item} />}
+      keyExtractor={item => item.id.toString()}
+      initialNumToRender={4}
+      maxToRenderPerBatch={2}
+      renderItem={renderItem}
     />
   );
 };
