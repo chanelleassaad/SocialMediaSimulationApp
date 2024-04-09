0239425f812266c64684c622baa31a57e84bf332
Chanelle Assaad <assaadchanelle@gmail.com>
2024-04-09 17:19:00 +0300

    Fix signup bug

--- src/config/UserApi.tsx
+++ src/config/UserApi.tsx
@@ -18,28 +18,34 @@ export const loginUser = async (username, password) => {
 
 export const signUpUser = async (username, email, password, isAdmin) => {
   try {
-    // Check if email is already in use
-    const emailResponse = await api.get(`/users?email=${email}`);
-    if (emailResponse.data.length > 0) {
-      throw new Error('Email already in use');
+    let createUser = false;
+    try {
+      // Check if username is already in use
+      const usernameResponse = await api.get(`/users?username=${username}`);
+      if (usernameResponse.data.length > 0) {
+        throw new Error('Username already in use');
+      }
+
+      // Check if email is already in use
+      const emailResponse = await api.get(`/users?email=${email}`);
+      if (emailResponse.data.length > 0) {
+        throw new Error('Email already in use');
+      }
+    } catch (error) {
+      createUser = true;
     }
 
-    // Check if username is already in use
-    const usernameResponse = await api.get(`/users?username=${username}`);
-    if (usernameResponse.data.length > 0) {
-      throw new Error('Username already in use');
+    // Create the new user if username is available
+    if (createUser) {
+      const user = {
+        username: username,
+        email: email,
+        password: password,
+        isAdmin: isAdmin,
+      };
+      const newUserResponse = await api.post('/users', user);
+      return newUserResponse.data;
     }
-
-    // Create the new user
-    const user = {
-      username: username,
-      email: email,
-      password: password,
-      isAdmin: isAdmin,
-    };
-    const newUserResponse = await api.post('/users', user);
-
-    return newUserResponse.data;
   } catch (error) {
     throw error;
   }
